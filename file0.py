46327d631fd0d07de119bb30370a325db122179c
Martin Faucheux <martin.faucheux@gitguardian.com>
2023-01-23 16:32:06 +0100

    allow to commit in another repo

--- file0.py
+++
@@ -1,1 +0,0 @@
-# This is the content of the file for commit 1
--- git_utils.py
+++ git_utils.py
@@ -8,18 +8,24 @@ def push_change(file_path, commit_id):
     header_commands = []
     if ado_pat := os.environ.get("ADO_PAT"):
         b64token = base64.b64encode(f":{ado_pat}".encode()).decode()
-        header_commands = ["-c", f'http.extraHeader="Authorization: Basic {b64token}"']
+        header_commands = ["-c", f"http.extraHeader=Authorization: Basic {b64token}"]
+
+    kwargs = dict()
+    if repo_path := os.environ.get("LOCAL_REPO_PATH", ""):
+        kwargs["cwd"] = repo_path
 
     # Modify the file
-    with open(file_path, "w") as f:
+    with open(os.path.join(repo_path, file_path), "w") as f:
         file_content = f"# This is the content of the file for commit {commit_id}"
         f.write(file_content)
 
     # Stage the file
-    subprocess.run(["git", "add", file_path], stdout=subprocess.DEVNULL)
+    subprocess.run(["git", "add", file_path], stdout=subprocess.DEVNULL, **kwargs)
 
     # Commit the file
-    subprocess.run(["git", "commit", "-q", "-m", f"Automatic commit {commit_id}"])
+    subprocess.run(
+        ["git", "commit", "-q", "-m", f"Automatic commit {commit_id}"], **kwargs
+    )
 
     # Push the commit
-    subprocess.run(["git", *header_commands, "push", "-q"])
+    subprocess.run(["git", *header_commands, "push", "-q"], **kwargs)
